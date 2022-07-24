0e4f099af341ed4e900a328dc8618f33e468b017
akgnyuksel <yuksel.anka@gmail.com>
2022-07-24 18:49:37 +0300

    Initial Commit

---
+++ src/constructors/Employee.java
@@ -0,0 +1,40 @@
+package constructors;
+
+public class Employee {
+
+    public String name;
+    public String jobTitle;
+    public int ID;
+    public double salary;
+
+    public Employee(){
+
+    }
+
+    public Employee(String name, String jobTitle, int ID, double salary) {
+        this.name = name;
+        this.jobTitle = jobTitle;
+        this.ID = ID;
+        this.salary = salary;
+    }
+
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", ID=" + ID +
+                ", salary=" + salary +
+                '}';
+    }
+}
+/*
+``Task 1:
+Employee:
+    attribute/fields/variables
+name, jobTitle, ID, salary
+
+    add a constructor to initialize all the fields
+    add a default constructor with no fields
+
+    toString() method will rint out the fields
+ */
